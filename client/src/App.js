73e9451b0422ab2dce5f03a3fe54bf44ebbc983f
dahakagh <Marshev123@yandex.ru>
2021-07-04 21:02:15 +0300

    longpulling and eventsourcing

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+//import LongPulling from "./LongPulling";
+import EventSourcing from "./EventSourcing";
+
+function App() {
+  return (
+    <div>
+      {/* <LongPulling /> */}
+      <EventSourcing />
+    </div>
+  );
+}
+
+export default App;
